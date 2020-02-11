873fd03663d31926c338b3dbb96411352679bea7
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-02-10 17:29:41 -0800

    Changed game to have 2 dices

--- app.js
+++ app.js
@@ -9,36 +9,32 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, diceDOM, gamePlaying, prevDice;
+var scores, roundScore, activePlayer, dice1DOM, dice2DOM, gamePlaying;
 
-diceDOM = document.querySelector('.dice');
+dice1DOM = document.getElementById('dice-1');
+dice2DOM = document.getElementById('dice-2');
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
   if (gamePlaying) {
     // 1. Random number
-    var dice = Math.floor(Math.random() * 6 + 1);
+    var dice1 = Math.floor(Math.random() * 6 + 1);
+    var dice2 = Math.floor(Math.random() * 6 + 1);
 
-    // 1.a Check if the player rolls two 6 in a row
-    if (dice === 6 && prevDice === 6){
-      scores[activePlayer] = 0;
-      switchPlayer();
+    // 2. Display the result
+    dice1DOM.style.display = 'block';
+    dice1DOM.src = 'dice-' + dice1 + '.png';
+    dice2DOM.style.display = 'block';
+    dice2DOM.src = 'dice-' + dice2 + '.png';
+
+    // 3. Update the round score IF the rolled number was not a 1
+    if (dice1 !== 1 && dice2 !== 1) {
+      // Add score
+      roundScore += dice1 + dice2;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-      prevDice = dice;
-
-      // 2. Display the result
-      diceDOM.style.display = 'block';
-      diceDOM.src = 'dice-' + dice + '.png';
-
-      // 3. Update the round score IF the rolled number was not a 1
-      if (dice !== 1) {
-        // Add score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-      } else {
-        switchPlayer();
-      }
+      switchPlayer();
     }
   }
 });
@@ -50,11 +46,20 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 
     // Update UI
     document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+    var winningScore;
+
+    var input = document.querySelector('.final-score').value;
+    if(input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
 
     // Check if player won the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= winningScore) {
       document.getElementById('name-' + activePlayer).textContent = 'Winner';
-      diceDOM.style.display = 'none';
+      dice1DOM.style.display = 'none';
+      dice2DOM.style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
       gamePlaying = false;
@@ -70,10 +75,10 @@ function init() {
   scores = [0, 0];
   roundScore = 0;
   activePlayer = 0;
-  prevDice = 0;
   gamePlaying = true;
 
-  diceDOM.style.display = 'none';
+  dice1DOM.style.display = 'none';
+  dice2DOM.style.display = 'none';
 
   for (var i = 0; i < 2; i++){
     document.getElementById('score-' + i).textContent = '0';
@@ -90,9 +95,9 @@ function init() {
 function switchPlayer() {
   // Reset dice and round score
   roundScore = 0;
-  prevDice = 0;
   document.getElementById('current-' + activePlayer).textContent = 0;
-  diceDOM.style.display = 'none';
+  dice1DOM.style.display = 'none';
+  dice2DOM.style.display = 'none';
   document.querySelector('.player-' +  activePlayer + '-panel').classList.remove('active');
 
   // Switch player
